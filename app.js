9f3b8a28309563a740be931c95aba4834755d19b
miracle ajayi <miracleajayi4@gmail.com>
2023-09-18 10:30:17 +0100

    added input field

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 
 
 app.get('/', (req, res) => {
-    res.send("<h2>Node app application testing</h2>")
+    res.send("<h2>Node app application testing</h2></br><input type='text' />")
 })
 
 app.listen('80', console.log('Server is listening on port 80...'))
