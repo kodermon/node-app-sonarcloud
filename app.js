5c304e77fd3ec15c862be9b82c41f6b5aee6415f
miracle ajayi <miracleajayi4@gmail.com>
2023-09-18 09:51:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send("<h2>Node app application testing</h2>")
+})
+
+app.listen('80', console.log('Server is listening on port 80...'))
